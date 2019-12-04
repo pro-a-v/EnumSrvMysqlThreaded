7c05bee2063082cd69112919db47c10706855d92
a.prochakovsky <prochakovsky@gmail.com>
2019-12-04 16:09:16 +0200

    just extra log

--- ProcessingTypeContainer.cpp
+++ ProcessingTypeContainer.cpp
@@ -11,11 +11,17 @@ std::string ProcessingTypeContainer::getType(std::string phone)
 {
 
     uint64_t int_phone = boost::lexical_cast<uint64_t>(phone);
-
+    LOG(INFO) << "uint64_t int_phone -" << int_phone;
     for (const auto &Record : ProcessingTypeData)
     {
+        LOG(INFO) << Record.from << int_phone << Record.to;
         if ( (Record.from >= int_phone ) &&  (Record.to <= int_phone) )
+        {
+
             return Record.type;
+
+
+        }
     }
 
     return std::string("DB");
